545d5e2318a8d241f121cff27a26d79d95493621
Mark Bieganek <mark.bieganek@telus.com>
2022-12-13 18:59:30 -0700

    Write db schema for properties table

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,8 +1,27 @@
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS properties CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255),
   password VARCHAR(255)
+);
+
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  thumbnail_photo_url VARCHAR(255),
+  cover_photo_url VARCHAR(255),
+  cost_per_night INTEGER CONSTRAINT valid_cost_per_night CHECK(cost_per_night >= 0) DEFAULT 0,
+  parking_spaces INTEGER DEFAULT 0,
+  number_of_bathrooms INTEGER DEFAULT 0,
+  number_of_bedrooms INTEGER DEFAULT 0,
+  country VARCHAR(255),
+  street VARCHAR(255),
+  province VARCHAR(255),
+  post_code VARCHAR(255),
+  active BOOLEAN DEFAULT FALSE
 );
