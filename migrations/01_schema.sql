1ed4bc68ab3313dbe79f6408f303ed7266c8fdbc
Mark Bieganek <mark.bieganek@telus.com>
2022-12-13 19:12:17 -0700

    Fix: schema to align with Compass outline

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -6,8 +6,8 @@ DROP TABLE IF EXISTS property_reviews CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  email VARCHAR(255),
-  password VARCHAR(255)
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE properties (
@@ -21,19 +21,19 @@ CREATE TABLE properties (
   parking_spaces INTEGER DEFAULT 0,
   number_of_bathrooms INTEGER DEFAULT 0,
   number_of_bedrooms INTEGER DEFAULT 0,
-  country VARCHAR(255),
-  street VARCHAR(255),
-  province VARCHAR(255),
-  post_code VARCHAR(255),
-  active BOOLEAN DEFAULT FALSE
+  country VARCHAR(255) NOT NULL,
+  street VARCHAR(255) NOT NULL,
+  province VARCHAR(255) NOT NULL,
+  post_code VARCHAR(255) NOT NULL,
+  active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
-  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
-  start_date DATE,
-  end_date DATE
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE property_reviews (
@@ -41,6 +41,6 @@ CREATE TABLE property_reviews (
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
-  rating SMALLINT NOT NULL CONSTRAINT valid_rating CHECK(rating >= 1 AND rating <= 5),
+  rating SMALLINT NOT NULL CONSTRAINT valid_rating CHECK(rating >= 1 AND rating <= 5) DEFAULT 5,
   message TEXT
 );
