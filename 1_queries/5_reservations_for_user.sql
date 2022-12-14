22f6ff1b8aa0c7bc017f7808b7dcdcdc49a0629c
Mark Bieganek <mark.bieganek@telus.com>
2022-12-14 09:47:00 -0700

    Add query to get reservations for a user

---
+++ 1_queries/5_reservations_for_user.sql
@@ -0,0 +1,18 @@
+SELECT
+  reservations.id,
+  properties.title,
+  properties.cost_per_night,
+  reservations.start_date,
+  AVG(property_reviews.rating) AS average_rating
+FROM reservations
+JOIN properties
+  ON properties.id = reservations.property_id
+JOIN property_reviews
+  ON property_reviews.property_id = reservations.property_id
+WHERE reservations.guest_id = 1
+GROUP BY
+  properties.id,
+  reservations.id
+ORDER BY reservations.start_date
+FETCH FIRST 10 ROWS ONLY
+;
